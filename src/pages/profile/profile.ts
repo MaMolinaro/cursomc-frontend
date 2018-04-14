6263a6964bc9ef6c986d339f58e8e717fcaa5d07
Marco Aurelio Molinaro <molinaromarcoaurelio@gmail.com>
2018-04-14 10:08:15 -0300

    Bugfix - atualização da imagem de perfil

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -5,6 +5,8 @@ import { ClienteDTO } from '../../models/cliente.dto';
 import { ClienteService } from '../../services/domain/cliente.service';
 import { API_CONFIG } from '../../config/api.config';
 import { CameraOptions, Camera } from '@ionic-native/camera';
+import { PromiseObservable } from 'rxjs/observable/PromiseObservable';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @IonicPage()
 @Component({
@@ -15,6 +17,7 @@ export class ProfilePage {
 
   cliente: ClienteDTO;
   picture: string;
+  profileImage;
   cameraOn: boolean = false;
 
   constructor(
@@ -22,7 +25,10 @@ export class ProfilePage {
         public navParams: NavParams,
         public storage: StorageService,
         public clienteService: ClienteService,
-        public camera: Camera) {
+        public camera: Camera,
+        public sanitizer: DomSanitizer) {
+
+          this.profileImage = 'assets/imgs/avatar-blank.png';
   }
 
   ionViewDidLoad() {
@@ -51,8 +57,23 @@ export class ProfilePage {
     this.clienteService.getImageFromBucket(this.cliente.id)
       .subscribe(response => {
         this.cliente.imageUrl = `${API_CONFIG.bucketBaseUrl}/cp${this.cliente.id}.jpg`;
+        this.blobToDataUrl(response).then(dataUrl => {
+          let str: string = dataUrl as string;
+          this.profileImage = this.sanitizer.bypassSecurityTrustUrl(str);
+        });
       },
-      error => {});
+      error => {
+        this.profileImage = 'assets/imgs/avatar-blank.png';
+      });
+  }
+
+  blobToDataUrl(blob) {
+    return new Promise((fullfill, reject) => {
+      let reader = new FileReader();
+      reader.onerror = reject;
+      reader.onload = (e) => fullfill(reader.result);
+      reader.readAsDataURL(blob);
+    })
   }
 
   getCameraPicture() {
@@ -97,7 +118,7 @@ export class ProfilePage {
     this.clienteService.uploadPicture(this.picture)
       .subscribe(response => {
         this.picture = null;
-        this.loadData();
+        this.getImageIfExists();
       }, 
       error => {
         this.cameraOn = false;
